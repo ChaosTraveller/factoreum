87a5372b99fe8c2236eb0a54ec9f11609aeb4376
ChaosTraveller <49252939+ChaosTraveller@users.noreply.github.com>
2019-04-05 21:35:57 +0200

    Okno programu

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,8 +1,27 @@
 package com.main;
 
-public class Main {
+import java.awt.*;
+
+public class Main extends Canvas implements Runnable {
+
+    public static final int Wi = 960, He = 720;
+
+    public Main(){
+        new Window(Wi, He, "Factoreum", this);
+
+    }
+
+    public synchronized void start() {
+
+    }
+
+    public void run(){
+
+    }
 
     public static void main(String[] args) {
-	// write your code here
+
+        new Main();
+
     }
 }
