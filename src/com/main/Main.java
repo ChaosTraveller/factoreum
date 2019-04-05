1def00d7b87fef874c05256f356fa3913dad76a0
ChaosTraveller <49252939+ChaosTraveller@users.noreply.github.com>
2019-04-05 22:26:44 +0200

    Główna pętla programu

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,21 +1,84 @@
 package com.main;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Main extends Canvas implements Runnable {
 
-    public static final int Wi = 960, He = 720;
+    public static final int wi = 960, he = 720;
+
+    private Thread action;
+    private boolean r = false;
 
     public Main(){
-        new Window(Wi, He, "Factoreum", this);
+        new Window(wi, he, "Factoreum", this);
 
     }
 
     public synchronized void start() {
 
+        action = new Thread(this);
+        action.start();
+        r = true;
+
+    }
+
+    public synchronized void stop() {
+
+        try {
+            action.join();
+            r = false;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void run(){
+
+        long lastTime = System.nanoTime();
+        double ticks = 30.0;
+        double ns = 1000000000 / ticks;
+        double d = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (r){
+            long now = System.nanoTime();
+            d += (now - lastTime) / ns;
+            lastTime = now;
+            while (d >= 1) {
+                tick();
+                d--;
+            }
+            if (r){
+                render();
+            }
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics gr = bs.getDrawGraphics();
+        gr.setColor(Color.black);
+        gr.fillRect(0,0, wi, he);
+        gr.dispose();
+        bs.show();
 
     }
 
